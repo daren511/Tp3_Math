fb353d2a041c657f262ba5b7d2cdc2df18c52723
Daren-Ken St-laurent <daren511@hotmail.com>
2014-05-08 16:32:01 -0400

    publish

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Tp3_Math
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LBL_PIMIN = new System.Windows.Forms.Label();
+            this.LBL_PIMAX = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LBL_PIMIN
+            // 
+            this.LBL_PIMIN.AutoSize = true;
+            this.LBL_PIMIN.Location = new System.Drawing.Point(107, 63);
+            this.LBL_PIMIN.Name = "LBL_PIMIN";
+            this.LBL_PIMIN.Size = new System.Drawing.Size(35, 13);
+            this.LBL_PIMIN.TabIndex = 0;
+            this.LBL_PIMIN.Text = "label1";
+            // 
+            // LBL_PIMAX
+            // 
+            this.LBL_PIMAX.AutoSize = true;
+            this.LBL_PIMAX.Location = new System.Drawing.Point(107, 88);
+            this.LBL_PIMAX.Name = "LBL_PIMAX";
+            this.LBL_PIMAX.Size = new System.Drawing.Size(35, 13);
+            this.LBL_PIMAX.TabIndex = 1;
+            this.LBL_PIMAX.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.LBL_PIMAX);
+            this.Controls.Add(this.LBL_PIMIN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LBL_PIMIN;
+        private System.Windows.Forms.Label LBL_PIMAX;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Tp3_Math
+{
+    
+    public partial class Form1 : Form
+    {
+        private double pchapeau = 0;
+        private double qchapeau = 0;
+        private double sigma = 0;
+        private int essaisdefaut = 1000;
+        private double erreur = 0;
+        private const double ZPOISSON = 2.575;
+        private double pmin = 0;
+        private double pmax = 0;
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculerPi(int essais)
+        {
+            Coordonnees coord;
+
+            for (int i = 0; i < essais; ++i)
+            {
+                coord = new Coordonnees(rand.NextDouble(), rand.NextDouble());
+
+                if (Math.Sqrt(Math.Pow(coord.X, 2) + Math.Pow(coord.Y, 2)) <= 1)
+                {
+                    pchapeau++;
+                }
+                else
+                {
+                    qchapeau++;
+                }
+            }
+
+            pchapeau = pchapeau / essais;
+            qchapeau = qchapeau / essais;
+
+            sigma = Math.Sqrt((pchapeau * qchapeau) / essais);
+
+            erreur = ZPOISSON * sigma;
+
+            pmin = pchapeau - erreur;
+            pmax = pchapeau + erreur;
+
+            LBL_PIMIN.Text = (4 * pmin).ToString();
+            LBL_PIMAX.Text = (4 * pmax).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            calculerPi(essaisdefaut);
+        }
+    }
+    public class Coordonnees
+    {
+        private double _X;
+        private double _Y;
+
+        public double X { get { return _X; } private set { this._X = value; } }
+        public double Y { get { return _Y; } private set { this._Y = value; } }
+
+        public Coordonnees(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
